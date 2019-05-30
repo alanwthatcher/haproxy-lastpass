f439ff368c023fd6b0f8a653430be6fae51c364e
Alan Thatcher <alanwthatcher@gmail.com>
2019-05-29 22:47:00 -0400

    initial commit of files
    Signed-off-by: Alan Thatcher <alanwthatcher@gmail.com>

---
+++ plan.sh
@@ -0,0 +1,48 @@
+pkg_name=haproxy-lastpass
+pkg_distname=haproxy
+pkg_origin=core
+pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
+pkg_version=1.9.6
+pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_license=('GPL-2.0' 'LGPL-2.1')
+pkg_source="https://www.haproxy.org/download/1.9/src/haproxy-${pkg_version}.tar.gz"
+pkg_upstream_url="https://www.haproxy.org/"
+pkg_shasum=0837c35e2914d40f685740487886e86b1b16132b81ecb60a3be66cf9a6f19bef
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_svc_user=root
+pkg_svc_group=root
+pkg_exports=(
+  [port]=front-end.port
+  [status-port]=status.port
+)
+pkg_exposes=(port status-port)
+pkg_binds=(
+  [backend]="port"
+)
+pkg_deps=(core/zlib core/pcre core/openssl core/jq-static nala/lastpass-cli)
+pkg_build_deps=(
+  core/coreutils
+  core/gcc
+  core/pcre
+  core/make
+  core/openssl
+  core/zlib
+  core/diffutils
+)
+pkg_bin_dirs=(bin)
+
+do_build() {
+  make USE_PCRE=1 \
+       USE_PCRE_JIT=1 \
+       TARGET=linux2628 \
+       USE_OPENSSL=1 \
+       USE_ZLIB=1 \
+       USE_GETADDRINFO=1 \
+       ADDINC="${CFLAGS}" \
+       ADDLIB="${LDFLAGS}"
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}"/bin
+  cp haproxy "${pkg_prefix}"/bin
+}
